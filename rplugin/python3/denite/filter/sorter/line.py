0924d2674319fc24ead6c41e42c23d676894d205
ps4515 <simatosp@gmail.com>
2018-09-19 18:44:27 +0100

    Seperate filename and linenumber sorters. Short documentation added.

--- rplugin/python3/denite/filter/sorter/line.py
+++ rplugin/python3/denite/filter/sorter/line.py
@@ -1,7 +1,7 @@
 # ============================================================================
-# FILE: sorter/filename.py
+# FILE: sorter/line.py
 # AUTHOR: PanagiotisS
-# DESCRIPTION: Simple filter to sort candidates by ascii order of file name.
+# DESCRIPTION: Simple filter to sort candidates by line number.
 #              Based on sorter/word.py from Tomohito OZAKI.
 # License: MIT license
 # ============================================================================
@@ -13,8 +13,9 @@ class Filter(Base):
     def __init__(self, vim):
         super().__init__(vim)
 
-        self.name = 'sorter/filename'
-        self.description = 'sort candidates by ascii order of file name'
+        self.name = 'sorter/line'
+        self.description = 'sort candidates by the line number'
 
     def filter(self, context):
-        return sorted(context['candidates'], key=lambda x: x['action__path'])
+        return sorted(context['candidates'],
+                key=lambda x: int(x['action__line']))
